7f99007fd59e51273039c9e34b30ca44a2a11b34
Artem Jheleznov <52380931+Reverlight@users.noreply.github.com>
2022-03-31 13:58:46 +0300

    Upvotes_amount to upvote_amount renamed

--- news/models.py
+++ news/models.py
@@ -5,5 +5,5 @@ class NewsPost(models.Model):
     title = models.CharField(max_length=255)
     link = models.URLField()
     created_at = models.DateTimeField(auto_now_add=True)
-    upvotes_amount = models.IntegerField()
+    upvote_amount = models.IntegerField(blank=True, default=0)
     author_name = models.CharField(max_length=255)
