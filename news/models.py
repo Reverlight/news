493de6454d14fd62eab8ba98bcc7e5a13caece0c
Artem Jheleznov <52380931+Reverlight@users.noreply.github.com>
2022-03-31 13:26:49 +0300

    News Post Model

--- news/models.py
+++ news/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class NewsPost(models.Model):
+    title = models.CharField(max_length=255)
+    link = models.URLField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    upvotes_amount = models.IntegerField()
+    author_name = models.CharField(max_length=255)
