7094011896af0f725352b311015a5cbe76d40625
Artem Jheleznov <52380931+Reverlight@users.noreply.github.com>
2022-03-31 16:42:45 +0300

    Implement comments
    * Comment serializer
    
    * Comment urls
    
    * Comment view set

--- news/serializers.py
+++ news/serializers.py
@@ -1,12 +1,21 @@
 from rest_framework import serializers
 
-from .models import NewsPost
+from .models import NewsPost, Comment
 
 
 class NewsPostSerializer(serializers.ModelSerializer):
+    pk = serializers.IntegerField(read_only=True)
     upvote_amount = serializers.IntegerField(read_only=True)
     created_at = serializers.DateTimeField(read_only=True)
 
     class Meta:
         model = NewsPost
-        fields = ['title', 'link', 'author_name', 'created_at', 'upvote_amount']
+        fields = ['title', 'link', 'author_name', 'created_at', 'upvote_amount', 'pk']
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    pk = serializers.IntegerField(read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = ['post', 'author_name', 'content', 'created_at', 'pk']
