709799b53fff97d5b03f39cbc8628b12bbe80af6
Artem Jheleznov <52380931+Reverlight@users.noreply.github.com>
2022-03-31 14:21:21 +0300

    Comment view set

--- news/views.py
+++ news/views.py
@@ -1,9 +1,14 @@
 from rest_framework import viewsets
 
-from news.models import NewsPost
-from news.serializers import NewsPostSerializer
+from news.models import NewsPost, Comment
+from news.serializers import NewsPostSerializer, CommentSerializer
 
 
 class NewsPostViewSet(viewsets.ModelViewSet):
     queryset = NewsPost.objects.all()
     serializer_class = NewsPostSerializer
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
