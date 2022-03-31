1e5a8f49c8e8f962b498e03b1b2063cb95917796
Artem Jheleznov <52380931+Reverlight@users.noreply.github.com>
2022-03-31 13:59:45 +0300

    News post model view set

--- news/views.py
+++ news/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from news.models import NewsPost
+from news.serializers import NewsPostSerializer
+
+
+class NewsPostViewSet(viewsets.ModelViewSet):
+    queryset = NewsPost.objects.all()
+    serializer_class = NewsPostSerializer
+
