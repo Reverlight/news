f70a85205bff01685952cf38de39f1bf7c469c88
Artem Jheleznov <52380931+Reverlight@users.noreply.github.com>
2022-03-31 14:27:10 +0300

    Included pk to be visible

--- news/serializers.py
+++ news/serializers.py
@@ -4,15 +4,18 @@ from .models import NewsPost, Comment
 
 
 class NewsPostSerializer(serializers.ModelSerializer):
+    pk = serializers.IntegerField(read_only=True)
     upvote_amount = serializers.IntegerField(read_only=True)
     created_at = serializers.DateTimeField(read_only=True)
 
     class Meta:
         model = NewsPost
-        fields = ['title', 'link', 'author_name', 'created_at', 'upvote_amount']
+        fields = ['title', 'link', 'author_name', 'created_at', 'upvote_amount', 'pk']
 
 
 class CommentSerializer(serializers.ModelSerializer):
+    pk = serializers.IntegerField(read_only=True)
+
     class Meta:
         model = Comment
-        fields = ['post', 'author_name', 'content', 'created_at']
+        fields = ['post', 'author_name', 'content', 'created_at', 'pk']
